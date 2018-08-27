3efd423781ee02b94e9e6421e3fbaebd74218773
Andrew Mainhart <andrewmainhart426@gmail.com>
2018-08-27 14:58:50 -0400

    Templates for checkers game screen and checkers board.

---
+++ src/app/checker-board/checker-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checker-board',
+  templateUrl: './checker-board.component.html',
+  styleUrls: ['./checker-board.component.css']
+})
+export class CheckerBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
