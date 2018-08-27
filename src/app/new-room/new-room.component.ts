37219faf4b3a4abf69693b78cc8c597d23b7fadb
Andrew Mainhart <andrewmainhart426@gmail.com>
2018-08-27 14:34:17 -0400

    Initial user checking.

--- src/app/new-room/new-room.component.ts
+++ src/app/new-room/new-room.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {RoomService} from "../room.service";
 import {Room} from "../types/Room";
 import {UserService} from "../user.service";
+import {User} from "../types/User";
 
 @Component({
   selector: 'app-new-room',
@@ -14,15 +15,18 @@ export class NewRoomComponent implements OnInit {
               private userService: UserService) { }
 
   room: Room = null;
+  currentUser: User = null;
 
   ngOnInit() {
+    this.userService.getUser().subscribe((value) => {
+      this.currentUser = value;
+    });
   }
 
   newRoom() {
-    this.userService.setUser().subscribe(value => {
       this.roomService.newRoom().subscribe((value => {
         this.room = value;
       }));
-    });
+
   }
 }
