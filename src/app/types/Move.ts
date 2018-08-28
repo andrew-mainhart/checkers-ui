c32cec43c6e998f58bb6fea940c250d785f17092
Andrew Mainhart <andrewmainhart426@gmail.com>
2018-08-27 21:55:08 -0400

    Adding fields to Move.ts

--- src/app/types/Move.ts
+++ src/app/types/Move.ts
@@ -1,3 +1,11 @@
-export class Move {
+import {Chip} from "./Chip";
+import {Coord} from "./Coord";
+import {User} from "./User";
 
+export class Move {
+  chip: Chip;
+  fromSpot: Coord;
+  toSpot: Coord;
+  intermediateSpots: Coord[];
+  byPlayer: User;
 }
